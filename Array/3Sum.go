3d65512f487dabdf410ba8409d8e412e276333c0
celeste0922 <2895751296@qq.com>
2023-10-26 15:04:36 +0800

    2023.10.26 optimize 3Sum

--- Array/3Sum.go
+++ Array/3Sum.go
@@ -7,6 +7,12 @@ import "sort"
 // 第一个数作为第一层循环
 // 第二个数和第三个数作为第二层循环的双指针
 // 第二层遇到相同的下个数自动跳过避免重复
+//=========================================
+//设 s = nums[first] + nums[first+1] + nums[first+2]，如果 s > 0，由于数组已经排序，后面无论怎么选，选出的三个数的和不会比 s 还小，所以只要 s > 0 就可以直接 break 外层循环了。
+//
+//如果 nums[first] + nums[n-2] + nums[n-1] < 0，由于数组已经排序，nums[first] 加上后面任意两个数都是小于 0 的，所以下面的双指针就不需要跑了。但是后面可能有更大的 nums[first]，所以还需要继续枚举，continue 外层循环。
+//
+//加上这两个优化后，就可以让代码击败接近 100%
 
 func ThreeSum(nums []int) [][]int {
 	i := 0
@@ -16,6 +22,9 @@ func ThreeSum(nums []int) [][]int {
 	sums := []int{}
 	sort.Ints(nums)
 	for ; i < len(nums)-2; i++ {
+		if nums[i]+nums[i+1]+nums[i+2] > 0 || nums[i]+nums[len(nums)-2]+nums[len(nums)-1] < 0 {
+			break
+		}
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
