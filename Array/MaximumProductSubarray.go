0766beb1651be54874c40dfc44f28861307209ab
celeste0922 <2895751296@qq.com>
2023-11-16 10:54:10 +0800

    commit MaximumProductSubarray -动态规划！

--- Array/MaximumProductSubarray.go
+++ Array/MaximumProductSubarray.go
@@ -1,7 +1,5 @@
 package Array
 
-import "fmt"
-
 //152. 乘积最大子数组
 //给你一个整数数组 nums ，请你找出数组中乘积最大的非空连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
 //测试用例的答案是一个 32-位 整数。
@@ -18,7 +16,6 @@ func MaxProduct(nums []int) int {
 		return nums[0]
 	}
 	for i := 1; i < n; i++ {
-		fmt.Println(nums)
 		minNum, maxNum = min(min(minNum*nums[i], nums[i]), maxNum*nums[i]), max(max(maxNum*nums[i], nums[i]), minNum*nums[i])
 		res = max(res, maxNum)
 	}
