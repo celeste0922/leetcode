4cf7def00026fcea8f079c018e2aff651a630ca2
celeste0922 <2895751296@qq.com>
2023-11-20 16:03:05 +0800

    commit MoveZeroes -简单快慢指针

--- main.go
+++ main.go
@@ -78,6 +78,8 @@ func main() {
 	//fmt.Println(Array.CombinationSum3(3, 9))
 	//fmt.Print("存在重复元素===>")
 	//fmt.Println(Array.ContainsDuplicate([]int{1, 2, 3, 1}))
-	fmt.Print("存在重复元素II===>")
-	fmt.Println(Array.ContainsNearbyDuplicate([]int{1, 0, 1, 1}, 1))
+	//fmt.Print("存在重复元素II===>")
+	//fmt.Println(Array.ContainsNearbyDuplicate([]int{1, 0, 1, 1}, 1))
+	fmt.Print("移动零===>")
+	Array.MoveZeroes([]int{0, 1, 0, 3, 12})
 }
